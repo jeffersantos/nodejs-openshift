adeabb3340ad4e06e47a3f98553029d2db6adf33
jeffersantos <jeffersantos@gmail.com>
2020-04-18 15:31:44 -0300

    modify port database server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mysql = require ('mysql');
 
 var con= mysql.createConnection({
 	host: process.env.MYSQL_HOST,
+	port: process.env.MYSQL_PORT,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASS,
 	database: process.env.MYSQL_DATABASE
