5859d3408be0a855215b1be0ae42ba17553e08a6
jeffersantos <jeffersantos@gmail.com>
2020-04-18 15:42:39 -0300

    Ok

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var con= mysql.createConnection({
 	host: process.env.MYSQL_HOST,
 	port: process.env.MYSQL_PORT,
 	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASS,
+	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE
 });
 var nodemailer = require('nodemailer');
