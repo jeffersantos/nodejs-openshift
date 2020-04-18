d2e1e82134eebeaf46e888fe045c7ae32f754b14
jeffersantos <jeffersantos@gmail.com>
2020-04-18 15:06:28 -0300

    OK

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql = require ('mysql');
+
+var con= mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASS,
+	database: process.env.MYSQL_DATABASE
+});
+var nodemailer = require('nodemailer');
+
+var account = require('./routes/account');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());//parse html,json parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  req.con = con;
+  next();
+});
+
+app.use('/', account);//home
+app.use('/account', account);
+
+//catch 404 and forward to errorhandler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+app.listen(8080,'0.0.0.0');
